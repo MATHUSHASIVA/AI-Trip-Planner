254409b7a7958af50e380dc1e02a989e2c7ffa09
MATHUSHASIVA <mathushasiva2k@gmail.com>
2025-09-07 12:27:57 +0530

    Remove .env and .env.name files containing secrets

--- utils/place_info_search.py
+++ utils/place_info_search.py
@@ -1,5 +1,6 @@
 import os
 import json
+from unittest import result
 from langchain_tavily import TavilySearch
 from langchain_google_community import GooglePlacesTool, GooglePlacesAPIWrapper 
 
@@ -33,15 +34,16 @@ class GooglePlaceSearchTool:
         return self.places_tool.run(f"What are the different modes of transportations available in {place}")
 
 class TavilyPlaceSearchTool:
-    def __init__(self):
-        pass
+    def __init__(self, api_key: str = None):
+        self.api_key = api_key  
+    
 
     def tavily_search_attractions(self, place: str) -> dict:
         """
         Searches for attractions in the specified place using TavilySearch.
         """
-        tavily_tool = TavilySearch(topic="general", include_answer="advanced")
-        result = tavily_tool.invoke({"query": f"top attractive places in and around {place}"})
+        tavily_tool = TavilySearch(topic="general", include_answer="advanced", api_key=self.api_key)
+        result = tavily_tool.invoke({"query": f"what are the top 10 restaurants and eateries in and around {place}."})
         if isinstance(result, dict) and result.get("answer"):
             return result["answer"]
         return result
